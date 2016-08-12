0214ff5aa685b23ca9b62b1f3ba76d3e1cb42389
asouza <alots.ssa@gmail.com>
2016-08-12 14:57:48 -0300

    deixando o layout do cadastro okk

--- src/App.js
+++ src/App.js
@@ -16,36 +16,58 @@ class App extends Component {
 
                     <ul className="pure-menu-list">
                         <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autores</a></li>
+                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livros</a></li>
                     </ul>
                 </div>
             </div>
 
-            <div id="main">
-                <div className="header">
-                    <h1>Page Title</h1>
-                    <h2>A subtitle for your page goes here</h2>
-                </div>
-
-                <div className="content" id="root">
-                    <h2 className="content-subhead">How to use this layout</h2>
-                    <p>
-                        To use this layout, you can just copy paste the HTML, along with the CSS in <a href="/css/layouts/side-menu.css" alt="Side Menu CSS">side-menu.css</a>, and the JavaScript in <a href="/js/ui.js">ui.js</a>. The JS file uses vanilla JavaScript to simply toggle an <code>active</code> className that makes the menu responsive.
-                    </p>
-
-                    <h2 className="content-subhead">Now Lets Speak Some Latin</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-
-                    <div className="pure-g">
-                    </div>
-
-                    <h2 className="content-subhead">Try Resizing your Browser</h2>
-                    <p>
-                        
-                    </p>
-                </div>
+          <div id="main">
+            <div className="header">
+              <h1>Cadastro de Autores</h1>
             </div>
+            <div className="content" id="content">
+              <div className="pure-form pure-form-aligned">
+                <form className="pure-form pure-form-aligned">
+                  <div className="pure-control-group">
+                    <label htmlFor="nome">Nome</label> 
+                    <input id="nome" type="text" name="nome" value=""  />                  
+                  </div>
+                  <div className="pure-control-group">
+                    <label htmlFor="email">Email</label> 
+                    <input id="email" type="email" name="email" value=""  />                  
+                  </div>
+                  <div className="pure-control-group">
+                    <label htmlFor="senha">Email</label> 
+                    <input id="senha" type="password" name="senha"  />                                      
+                  </div>
+                  <div className="pure-control-group">                                  
+                    <label></label> 
+                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+                  </div>
+                </form>             
+
+              </div>  
+              <div>            
+                <table className="pure-table">
+                  <thead>
+                    <tr>
+                      <th>Nome</th>
+                      <th>email</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Alberto</td>                
+                      <td>alberto.souza@caelum.com.br</td>                
+                    </tr>
+                  </tbody>
+                </table> 
+              </div>             
+            </div>
+          </div>            
+
+
         </div>
 
     );
