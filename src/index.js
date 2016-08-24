73583c9e19d2941b0aa0a3e7c849b8424a057653
asouza <alots.ssa@gmail.com>
2016-08-24 17:19:46 -0300

    indexRoute para exibir a home no caso do acesso ao index

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import AutorBox from './Autor';
+import Home from './Home';
 import './index.css';
-import {Router, Route} from 'react-router'
+import {Router, Route,IndexRoute} from 'react-router'
 import {browserHistory} from 'react-router'
 
 ReactDOM.render((
 	<Router history={browserHistory}>
     	<Route path="/" component={App}>
+    		<IndexRoute component={Home}/>
 	    	<Route path="/autor" component={AutorBox}/>
 	    	<Route path="/livro"/>    	
     	</Route>
