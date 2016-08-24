19b926090cb1a23542b11e4730c9b6a0f00c141d
asouza <alots.ssa@gmail.com>
2016-08-24 15:58:50 -0300

    configuracao padrao do router

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {Router, Route} from 'react-router'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+	<Router>
+    	<Route path="/" component={App}/>		
+    	<Route path="/autor"/>
+    	<Route path="/livro"/>    	
+    </Router>
+), document.getElementById('root'));
